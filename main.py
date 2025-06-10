fbfe6a77a3080cbd64e9d8100631a01072be5937
rlarkdud123 <sn2025016@shs.hs.kr>
2025-06-10 14:46:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import streamlit as st
 st.title("첫 app")
 st.write("hello")
+name=st.text_input("이름 입력")
+if name:
+  if name=="김가영":
+    st.successs("반갑습니다. 김가영님!")
+  else:
+    st.warning("누구세요?")
